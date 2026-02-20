d3e93045a4f100aff342e37c1bb791a75c7c12a3
Atul Sirsode <35775621+atul-sirsode@users.noreply.github.com>
2026-02-20 17:04:07 +0530

    Update env.ts

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,12 +1,19 @@
-﻿import dotenv from "dotenv"
+import dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 
 export const env = {
-    PORT: process.env.PORT || 4000,
+  PORT: process.env.PORT || 4000,
 
-    API_BASE: process.env.API_BASE!,
-    LOGIN_OTP_URL: process.env.LOGIN_OTP_URL!,
-    VERIFY_OTP_URL:process.env.VERIFY_OTP_URL!,
-    RC_DETAILS_URL: process.env.RC_DETAILS_URL!
-}
+  API_BASE: process.env.API_BASE!,
+  LOGIN_OTP_URL: process.env.LOGIN_OTP_URL!,
+  VERIFY_OTP_URL: process.env.VERIFY_OTP_URL!,
+  RC_DETAILS_URL: process.env.RC_DETAILS_URL!,
+
+  // Database configuration
+  DB_HOST: process.env.DB_HOST!,
+  DB_USER: process.env.DB_USER!,
+  DB_PASSWORD: process.env.DB_PASSWORD!,
+  DB_NAME: process.env.DB_NAME!,
+  DB_TABLE: process.env.DB_TABLE!,
+};
