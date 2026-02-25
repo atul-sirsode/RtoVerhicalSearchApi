9c43900fc0735ba11e10cfe29449912f6821d6fb
Atul Sirsode <35775621+atul-sirsode@users.noreply.github.com>
2026-02-25 10:45:49 +0530

    Add files via upload

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
-import express from "express";
+﻿import express from "express";
 import cors from "cors";
 
 import authRoutes from "./routes/auth.routes.js";
 import rcRoutes from "./routes/rc.routes.js";
+import rcV1Routes from "./routes/rc.v1.routes.js";
+import rcV2Routes from "./routes/rc.v2.routes.js";
 import fileHistoryRoutes from "./routes/file-history.routes.js";
 import { errorMiddleware } from "./middleware/error.middleware.js";
 import { swaggerSpec, swaggerUi } from "./config/swagger.js";
@@ -18,9 +20,15 @@ app.use(
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Original routes
 app.use("/api/auth", authRoutes);
 app.use("/api/rc", rcRoutes);
 app.use("/api/file-history", fileHistoryRoutes);
+
+// Versioned routes
+app.use("/api/v1/rc", rcV1Routes);
+app.use("/api/v2/rc", rcV2Routes);
+
 // Swagger documentation
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
